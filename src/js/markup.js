ddd44fdce9898f2b81152435721d8f2fb922ad6c
KulibabaYuliia <kulibaba120@gmail.com>
2023-10-02 18:00:40 +0300

    Update markup.js

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,11 +1,14 @@
+import spriteURL from './img/sprite.svg';
+import photoUrl from './img/rafiki.jpg';
+
 export function createMarkup({ drinkThumb, drink, description, _id }) {
   let markup = `<li class="cardlist-item" data-id=${_id}>
-        <img src="${drinkThumb}" class="cardlist-img" alt="${drink}" onerror="this.onerror=null;this.src='https://raw.githubusercontent.com/KulibabaYuliia/projecttest/main/src/img/rafiki.jpg';" width=300>
+        <img src="${drinkThumb}" class="cardlist-img" alt="${drink}" onerror="this.onerror=null;this.src='${photoUrl}';" width=300>
         <h3 class="cardlist-drink">${drink}</h3>
         <p class="cardlist-descr">${description}</p>
         <div class="cartlist-btns"><button class="cardlist-learn">learn more</button><button class="cardlist-fav"><svg
                 class="cardlist-svg" weight="18" height="18">
-                <use href="https://github.com/KulibabaYuliia/projecttest/blob/main/src/img/sprite.svg#heart"></use>
+                <use href="${spriteURL}#heart"></use>
             </svg></button></div>
         </li>`;
   return markup;
